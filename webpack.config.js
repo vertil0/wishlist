3749d194d8de7252aa51342b5d6371838308267a
Roman Gutsul <viridiprince@gmail.com>
2019-06-19 18:20:01 +0300

    -

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
+      template: "./public/index.html",
       filename: "./index.html"
     }), 
     new ExtractTextPlugin('css/mystyles.css')
