33bd1640c5fbab64bcb89f4ef546f6ec3cb756a7
Roman Gutsul <viridiprince@gmail.com>
2019-06-21 11:39:34 +0300

    -

--- src/App.js
+++ src/App.js
@@ -1,37 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import sheet from './modules/sheet/containers/sheet'
-const isAuthenticated = user => !!user
 class App extends Component {
   render() {
     return (
       <Router>
-        <Route path="/" exact component={test} />
-        <Route path="/sheet/" component={sheet} />
+        <Route path="/" exact component={sheet} />
       </Router>
     )
   }
-}
-
-function test() {
-  return (
-    <div className="columns" style={{
-      height: "100vh",
-      padding: "0",
-      flexWrap: "nowrap",
-      justifyContent: "center",
-      alignItems: "center"
-    }}>
-    <Link to="/sheet"> 
-    <div class="is-8">
-      <article class="tile is-child notification is-warning" style={{width: "200px"}}>
-          <p class="title">Создать</p>
-          <p class="subtitle">нового персонажа</p>
-        </article>
-        </div>
-        </Link>
-    </div>
-  )
 }
 
 export default App
