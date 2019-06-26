356e5b356ad9454980300727b770aab73deb679a
Roman Gutsul <viridiprince@gmail.com>
2019-06-26 18:37:48 +0300

    -

--- src/App.js
+++ src/App.js
@@ -11,62 +11,83 @@ class App extends Component {
   }
   componentWillMount() {
     axios.get('wishes.json').then(item => {
+      console.log(item)
       this.setState({ music: item.data.music })
     })
   }
 
   renderMusic = () => {
+    
+   const priority = {'1': "is-error", "2": "is-warning", "3": "is-success", "4": "is-primary"}
     return this.state.music.map(item => {
+      console.log(item.checked)
       return (
-        <div className="column is-one-third">
-          <div className="card">
-            <header className="card-header">
-              <p className="card-header-title">
-                {item.name}
-              </p>
-            </header>
-            <div className="card-image">
-              <figure className="image is-1by1">
-                <img src={item.img} alt="Placeholder image" />
-                <span className="tag is-success is-overlay is-centered" style={{ width: 70, margin: '5px 5px 0 auto', padding: 5 }}>{item.price}</span>
-              </figure>
-            </div>
-            <div className="card-content">
-              <div className="media">
-                <div className="media-content">
-                  <p className="subtitle is-6 has-text-justified">Artist: {item.artist}</p>
-                </div>
-              </div>
-              {item.comment && <div className="content">
-                {item.comment}
-              </div>
-              }
-            </div>
-            <footer className="card-footer">
-              <a href={item.link} className="card-footer-item ">Buy</a>
+        <tr className={"nes-text " + priority[item.priority]} >
+        <td>
+        <label>
+  <input type="checkbox" className="nes-checkbox" onClick={(e) => e.preventDefault()} checked={item.checked}/>
+  <span></span>
+</label>
+  </td>
 
-            </footer>
-          </div>
-        </div>)
+        <td>{item.name}</td>
+        <td>{item.artist}</td>
+        <td>{item.price}</td>
+      </tr>
+      )
     })
-  }
+}
   render() {
     console.log(this.state.music)
     return (
-      <div>
-        <div className="tabs">
-          <ul>
-            <li className={this.state.tab === '1' && "is-active"} onClick={() => this.changeTab('1')}><a>1</a></li>
-            <li className={this.state.tab === '2' && "is-active"} onClick={() => this.changeTab('2')}><a>2</a></li>
-            <li className={this.state.tab === '3' && "is-active"} onClick={() => this.changeTab('3')}><a>3</a></li>
-            <li className={this.state.tab === '4' && "is-active"} onClick={() => this.changeTab('4')}><a>4</a></li>
-          </ul>
-        </div>
-        <div className="columns is-multiline">
-          {this.state.music.length > 0 && this.renderMusic()}
-        </div>
+      <div><div className="nes-container with-title is-centered" id="technics">
+      <p className="title"><a href="#technics">#</a>Техника</p>
+      <p>"Подымая технику до уровня фантазии, не опусти фантазию до уровня техники."</p>
+          <p>Владимир Хочинский </p>
+    </div>
+    <br/>
+    <progress className="nes-progress" value={0} max={this.state.music.length }></progress>
+    <div className="nes-table-responsive">
+      <table className="nes-table is-bordered is-centered">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Название альбома</th>
+            <th>Исполнитель</th>
+            <th>Цена</th>
+          </tr>
+        </thead>
+        <tbody>
+          
+        </tbody>
+      </table>
+    </div>
+    <br/>
+        <div className="nes-container with-title is-centered" id="music">
+  <p className="title"><a href="#music">#</a>Музыка</p>
+  <p>"Биение сердца это первый источник музыки!"</p>
+      <p>Сияющий Сатомэ</p>
+</div>
+<br/>
+<progress className="nes-progress" value={0} max={this.state.music.length }></progress>
+<div className="nes-table-responsive">
+  <table className="nes-table is-bordered is-centered">
+    <thead>
+      <tr>
+        <th></th>
+        <th>Название альбома</th>
+        <th>Исполнитель</th>
+        <th>Цена</th>
+      </tr>
+    </thead>
+    <tbody>
+      {this.state.music && this.state.music.length > 0 && this.renderMusic()}
+    </tbody>
+  </table>
+</div>
+      {/* <img src="https://geekflare.com/wp-content/uploads/2018/10/uikit.png"  style={{imageRendering: "pixelated"}}></img> */}
       </div>
-    );
+    )
   }
 }
 
